6601b6f1005779f5656bff813038f77665f62904
Archie Isdiningrat <archie.isdiningrat@tiket.com>
2020-11-17 22:15:33 +0700

    finish base

--- src/methods/handlers.js
+++ src/methods/handlers.js
@@ -1,12 +1,13 @@
 'use strict';
 
+const { HttpError } = require('tymon');
 const gcloud = require('../modules/gcloud');
 
-exports.startVm = async (data, context) => {
-    const vmName = "vscode";
+const VM_NAME = process.env.VIRTUAL_MACHINE_NAME;
 
+exports.startVm = async (data, context) => {
     const zone = gcloud.getInstance();
-    await zone.vm(vmName).start();
+    await zone.vm(VM_NAME).start();
 
     return {
         message: "server is starting"
@@ -14,19 +15,50 @@ exports.startVm = async (data, context) => {
 }
 
 exports.stopVm = async (data, context) => {
-    const vmName = "vscode";
-
     const zone = gcloud.getInstance();
-    await zone.vm(vmName).stop();
+    await zone.vm(VM_NAME).stop();
 
     return {
         message: "server is stoping"
     }
 }
 
-exports.telegramHandler = async (data, context) => {
+exports.statusVm = async (data, context) => {
+    const zone = gcloud.getInstance();
+    const [payload] = await zone.vm(VM_NAME).get();
+    const { metadata } = payload;
     return {
-        data: data.body
+        message: `server is ${metadata.status.toLowerCase()}`,
+    }
+}
+
+
+exports.telegramHandler = async (data, context) => {
+    if (data.body.message.from.id !== +process.env.MASTER_ID) {
+        throw HttpError.UnauthorizedError("unidentified personel !")
+    }
+
+    let reply;
+    switch (data.body.message.text) {
+        case "/spin":
+            reply = (await exports.startVm()).message;
+            break;
+
+        case "/terminate":
+            reply = (await exports.stopVm()).message;
+            break;
+
+        case "/status":
+            reply = (await exports.statusVm()).message;
+            break;
+    
+        default:
+            reply = "unknown command"
+            break;
+    }
+
+    return {
+        message: reply
     };
 }
 
--- src/routes/main_routes.js
+++ src/routes/main_routes.js
@@ -1,6 +1,6 @@
 'use strict';
 const ApiGuard = require('../middlewares/api_guard');
-const { telegramHandler, startVm, stopVm } = require('../methods/handlers');
+const { telegramHandler, startVm, stopVm, statusVm } = require('../methods/handlers');
 const { ExpressLogicAdapter: Logic } = require('../utils/libs/express');
 
 const router = require('express').Router();
@@ -8,6 +8,8 @@ const router = require('express').Router();
 /** User Routes */
 router.post('/vm/start', ApiGuard, Logic(startVm));
 router.post('/vm/stop', ApiGuard, Logic(stopVm));
-router.post("/telegram", Logic(telegramHandler));
+router.post('/vm/status', ApiGuard, Logic(statusVm));
+router.post(`/bot/${process.env.TELEGRAM_KEY}`, Logic(telegramHandler));
 
 module.exports = router;
+
